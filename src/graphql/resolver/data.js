015a1d0cf8fd9afbf37e75b559142a68f9085161
Edgardo <ecofre@gmail.com>
2020-08-30 14:01:50 -0400

    Se agregan libros en archivo data.js

--- src/graphql/resolver/data.js
+++ src/graphql/resolver/data.js
@@ -12,7 +12,28 @@ const booksList = [
         author: 'Brian W. Kernighan, Dennis M. Ritchie',
         description: 'For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers. Includes detailed coverage of the C language plus the official C language reference manual for at-a-glance help with syntax notation, declarations, ANSI changes, scope rules, and the list goes on and on.',
         topic: 'Programming'
-    }
+    },
+    {
+        id: 3,
+        title: '1984',
+        author: 'George Orwell',
+        description: '1984 is George Orwell’s haunting prophecy of the future, which has held multiple generations of readers spellbound in its chilling and terrifying vision of life under a dictatorship. Powerful and unforgettable, this still-relevant novel, written in 1948, explores the obliteration of truth, individuality, and liberty in a world where the ruling power seeks to control everything, from information to thought and memory.',
+        topic: 'Fiction'
+    },
+    {
+        id:4,
+        title: "Emotional Intelligence",
+        author: "Daniel Goleman",
+        description: "Everyone knows that high IQ is no guarantee of success, happiness, or virtue, but until Emotional Intelligence, we could only guess why. Daniel Goleman's brilliant report from the frontiers of psychology and neuroscience offers startling new insight into our 'two minds'—the rational and the emotional—and how they together shape our destiny.",
+        topic: "Psychology"
+    },
+    { 
+        id:5, 
+        title: "Brave New World", 
+        author: "Aldous Huxley", 
+        description: "Largely set in a futuristic World State, whose citizens are environmentally engineered into an intelligence-based social hierarchy, the novel anticipates huge scientific advancements in reproductive technology, sleep-learning, psychological manipulation and classical conditioning that are combined to make a dystopian society which is challenged by only a single individual: the story's protagonist.",
+        topic: 'Fiction'
+    }    
 ]
 
 module.exports = booksList
