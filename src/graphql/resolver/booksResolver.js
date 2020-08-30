7aa8f35a763b292cbc12dd5d3189cda23e3438a9
Edgardo <ecofre@gmail.com>
2020-08-30 14:15:49 -0400

    Se agregan funciones getBook y getBooksByTopic en resolver

--- src/graphql/resolver/booksResolver.js
+++ src/graphql/resolver/booksResolver.js
@@ -4,8 +4,24 @@ const getBooks = (args) => {
     return booksList;
 }
 
+const getBook = (args) => { 
+    const id = args.id;
+    return booksList.find(book => book.id == id);
+}
+
+const getBooksByTopic = (args) => {
+    if (args.topic) {
+        const topic = args.topic;
+        return booksList.filter(book => book.topic === topic);
+    } else {
+        return booksList;
+    }
+}
+
 const booksResolver = {
-    books: getBooks
+    books: getBooks,
+    book: getBook,
+    booksByTopic: getBooksByTopic
 };
 
 module.exports = booksResolver
--- src/graphql/resolver/data.js
+++ src/graphql/resolver/data.js
@@ -1,24 +1,24 @@
 const booksList = [
     {
         id: 1,
-        title: 'A Brief History of Time',
-        author: 'Stephen Hawking',
-        description: 'Told in language we all can understand, A Brief History of Time plunges into the exotic realms of black holes and quarks, of antimatter and “arrows of time,” of the big bang and a bigger God—where the possibilities are wondrous and unexpected. With exciting images and profound imagination, Stephen Hawking brings us closer to the ultimate secrets at the very heart of creation.',
-        topic: 'Science'
+        title: "A Brief History of Time",
+        author: "Stephen Hawking",
+        description: "Told in language we all can understand, A Brief History of Time plunges into the exotic realms of black holes and quarks, of antimatter and “arrows of time,” of the big bang and a bigger God—where the possibilities are wondrous and unexpected. With exciting images and profound imagination, Stephen Hawking brings us closer to the ultimate secrets at the very heart of creation.",
+        topic: "Science"
     },
     {
         id: 2,
-        title: 'C Programming Language',
-        author: 'Brian W. Kernighan, Dennis M. Ritchie',
-        description: 'For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers. Includes detailed coverage of the C language plus the official C language reference manual for at-a-glance help with syntax notation, declarations, ANSI changes, scope rules, and the list goes on and on.',
-        topic: 'Programming'
+        title: "C Programming Language",
+        author: "Brian W. Kernighan, Dennis M. Ritchie",
+        description: "For years, C programmers have let K&R guide them to building well-structured and efficient programs. Now this same help is available to those working with ANSI compilers. Includes detailed coverage of the C language plus the official C language reference manual for at-a-glance help with syntax notation, declarations, ANSI changes, scope rules, and the list goes on and on.",
+        topic: "Programming"
     },
     {
         id: 3,
-        title: '1984',
-        author: 'George Orwell',
-        description: '1984 is George Orwell’s haunting prophecy of the future, which has held multiple generations of readers spellbound in its chilling and terrifying vision of life under a dictatorship. Powerful and unforgettable, this still-relevant novel, written in 1948, explores the obliteration of truth, individuality, and liberty in a world where the ruling power seeks to control everything, from information to thought and memory.',
-        topic: 'Fiction'
+        title: "1984",
+        author: "George Orwell",
+        description: "1984 is George Orwell’s haunting prophecy of the future, which has held multiple generations of readers spellbound in its chilling and terrifying vision of life under a dictatorship. Powerful and unforgettable, this still-relevant novel, written in 1948, explores the obliteration of truth, individuality, and liberty in a world where the ruling power seeks to control everything, from information to thought and memory.",
+        topic: "Fiction"
     },
     {
         id:4,
@@ -32,7 +32,7 @@ const booksList = [
         title: "Brave New World", 
         author: "Aldous Huxley", 
         description: "Largely set in a futuristic World State, whose citizens are environmentally engineered into an intelligence-based social hierarchy, the novel anticipates huge scientific advancements in reproductive technology, sleep-learning, psychological manipulation and classical conditioning that are combined to make a dystopian society which is challenged by only a single individual: the story's protagonist.",
-        topic: 'Fiction'
+        topic: "Fiction"
     }    
 ]
 
