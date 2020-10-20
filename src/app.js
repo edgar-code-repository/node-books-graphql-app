7f6ceb8694b06dc5b504a8f8dbf359f4f7529879
Edgardo Cofré Durán <ecofre@gmail.com>
2020-10-20 20:28:02 -0300

    Se agrega uso de libreria cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./graphql/schema/booksSchema');
 const booksResolver = require('./graphql/resolver/booksResolver');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/graphql', graphqlHTTP({
